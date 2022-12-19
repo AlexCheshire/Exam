6f12b4a65244d04784ab36abebd7d418db1f9af1
Kozachenko O <90053232+AlexCheshire@users.noreply.github.com>
2022-12-19 11:12:56 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import datetime
+from datetime import date
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    today = datetime.date.today()
+    month = today.month
+    quad = month//4 + 1
+    return quad
+
+if __name__ =='__main__':
+    app.run(debug=True,host='0.0.0.0')
---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+
+	def setUp(self):
+		self.app = tested_app.test_client()
+
+	def test_get_hello_endpoint(self):
+		r = self.app.get('/')
+		self.assertEqual(r.status, '200 OK')
+		self.assertEqual(r.data, 4)
+		self.assertIsNotNone(r.data)
+		self.assertLess(r.data, 5)
+		self.assertGreater(r.data, 0)
+
+if __name__ =='__main__':
+	import xmlrunner
+	runner = xmlrunner.XMLTestRunner(output='test-reporst')
+	unittest.main(testRunner=runner)
+	unittest.main()
